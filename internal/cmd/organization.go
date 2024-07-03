738bffc7dd135f42397be5ab9d0ef6491dd42013
Ron <rarevalo13@users.noreply.github.com>
2024-07-03 12:04:40 -0400

    Update internal/cmd/organization.go
    Co-authored-by: Karan Kajla <kkajla12@users.noreply.github.com>

--- internal/cmd/organization.go
+++ internal/cmd/organization.go
@@ -60,7 +60,7 @@ var createOrgCmd = &cobra.Command{
 }
 
 var updateOrgCmd = &cobra.Command{
-	Use:     "update <name> <domain> [state]",
+	Use:     "update <organizationId> <name> [domain] [state]",
 	Short:   "Update an organization",
 	Long:    "Update an organization with a specified name and domain. Optionally, specify the state of the domain (verified or pending).",
 	Example: "workos organization update FooCorp foo-corp.com pending",
