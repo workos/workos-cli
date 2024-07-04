b1a268f702d677345b4d7fecc42558ad2eea5e07
Rakesh Patel <roc@rocpatel.me>
2024-07-04 09:17:10 -0700

    Add charm library

--- internal/cmd/organization.go
+++ internal/cmd/organization.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 	"github.com/spf13/cobra"
 	"github.com/workos/workos-go/v4/pkg/organizations"
 )
 
 func init() {
 	orgCmd.AddCommand(createOrgCmd)
+	orgCmd.AddCommand(listOrgCmd)
 	rootCmd.AddCommand(orgCmd)
 }
 
@@ -17,6 +22,80 @@ var orgCmd = &cobra.Command{
 	Use:   "organization",
 	Short: "Manage organizations (create, update, delete, etc).",
 	Long:  "Create, update, and delete organizations and manage organization domain policies.",
+}
+
+// pass flags instead
+var listOrgCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List organizations with optional filters",
+	Long:  "List organizations, optionally filtering by domain, limit, before/after cursor, and order (asc/desc).",
+	Example: `workos organization list --domain foo-corp.com --limit 10 --before cursor --order desc
+workos organization list --domain foo-corp.com --after cursor --order asc`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Retrieve flag values
+		domain, _ := cmd.Flags().GetString("domain")
+		limit, _ := cmd.Flags().GetInt("limit")
+		before, _ := cmd.Flags().GetString("before")
+		after, _ := cmd.Flags().GetString("after")
+		order, _ := cmd.Flags().GetString("order")
+
+		var domains []string
+		if domain != "" {
+			domains = strings.Fields(domain)
+
+		}
+
+		var orgOrder organizations.Order
+		switch order {
+		case "asc":
+			orgOrder = organizations.Asc
+		case "desc":
+			orgOrder = organizations.Desc
+		default:
+			if order != "" {
+				return fmt.Errorf("invalid order value: must be 'asc' or 'desc'")
+			}
+		}
+
+		// Create the options struct
+		orgOpts := organizations.ListOrganizationsOpts{
+			Domains: domains,
+			Limit:   limit,
+			Before:  before,
+			After:   after,
+			Order:   orgOrder,
+		}
+
+		// List organizations
+		org, err := organizations.ListOrganizations(context.Background(), orgOpts)
+		if err != nil {
+			return fmt.Errorf("error listing organizations: %v", err)
+		}
+
+		s := lipgloss.NewStyle().Foreground(lipgloss.Color("#FFCC00")).Render
+		t := table.New().Border(lipgloss.NormalBorder()).Width(160).BorderHeader(true)
+		t.Headers(s("ID"), s("Name"), s("Domains"), s("Magic Auth"), s("Github Auth"), s("Google Auth"))
+		trueFalseSymbols := map[bool]string{true: "✅", false: "❌"}
+
+		for _, row := range org.Data {
+			domains := []string{}
+			for _, d := range row.Domains {
+				domains = append(domains, d.Domain)
+			}
+
+			t.Row(
+				row.ID,
+				row.Name,
+				strings.Join(domains, ", "),
+				trueFalseSymbols[row.MagicLinkAuthEnabled],
+				trueFalseSymbols[row.GithubOauthAuthEnabled],
+				trueFalseSymbols[row.GoogleOauthAuthEnabled],
+			)
+		}
+
+		fmt.Println(t.Render())
+		return nil
+	},
 }
 
 var createOrgCmd = &cobra.Command{
