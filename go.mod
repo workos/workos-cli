590dd3bb529aa08ebf22a944e417ec061bebd5e4
Karan Kajla <karan@warrant.dev>
2024-07-04 15:50:09 -0700

    Add support for fga check command

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/charmbracelet/huh v0.4.2
+	github.com/muesli/termenv v0.15.2
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 	github.com/workos/workos-go/v4 v4.14.0
@@ -23,6 +24,7 @@ require (
 	github.com/charmbracelet/x/input v0.1.1 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -37,8 +39,8 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
-	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -46,6 +48,7 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/atomic v1.9.0 // indirect
--- internal/cmd/fga.go
+++ internal/cmd/fga.go
@@ -2,9 +2,14 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/muesli/termenv"
 	"github.com/spf13/cobra"
+	"github.com/workos/workos-cli/internal/printer"
 	"github.com/workos/workos-go/v4/pkg/fga"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,30 +21,40 @@ func init() {
 	// warrants
 	fgaCmd.AddCommand(assignRelationCmd)
 	fgaCmd.AddCommand(removeRelationCmd)
+
+	// check
 	fgaCmd.AddCommand(checkRelationCmd)
-	checkRelationCmd.Flags().StringP("warrantToken", "w", "", "")
-	fgaCmd.AddCommand(queryCmd)
-	queryCmd.Flags().StringP("warrantToken", "w", "", "")
+	checkRelationCmd.Flags().StringP("warrantToken", "w", "", "warrant token to use for check")
+	checkRelationCmd.Flags().String("assert", "", "assert that the check is true or false")
+	checkRelationCmd.Flags().BoolP("debug", "d", false, "run check in debug mode")
 
 	// objects
 	fgaCmd.AddCommand(createObjectCmd)
 	fgaCmd.AddCommand(listObjectsCmd)
-	listObjectsCmd.Flags().String("objectType", "", "The type of objects to filter by")
-	listObjectsCmd.Flags().String("search", "", "The search term to filter a list of results by")
-	listObjectsCmd.Flags().Int("limit", 10, "Limit the number of results returned")
-	listObjectsCmd.Flags().String("before", "", "A cursor indicating results that occur before a specific result")
-	listObjectsCmd.Flags().String("after", "", "A cursor indicating results that occur after a specific result")
-	listObjectsCmd.Flags().String("order", "", "The order in which a list of results should be returned (asc or desc)")
+	listObjectsCmd.Flags().String("type", "", "object type to filter results by")
+	listObjectsCmd.Flags().String("search", "", "search term to filter a list of results by")
+	listObjectsCmd.Flags().Int("limit", 10, "limit the number of results returned")
+	listObjectsCmd.Flags().String("before", "", "cursor indicating results that occur before a specific result")
+	listObjectsCmd.Flags().String("after", "", "cursor indicating results that occur after a specific result")
+	listObjectsCmd.Flags().String("order", "", "order in which a list of results should be returned (asc or desc)")
 	fgaCmd.AddCommand(updateObjectCmd)
 	fgaCmd.AddCommand(deleteObjectCmd)
+
+	// query
+	fgaCmd.AddCommand(queryCmd)
+	queryCmd.Flags().StringP("warrantToken", "w", "", "warrant token to use for query")
+	queryCmd.Flags().Int("limit", 10, "limit the number of results returned")
+	queryCmd.Flags().String("before", "", "cursor indicating results that occur before a specific result")
+	queryCmd.Flags().String("after", "", "cursor indicating results that occur after a specific result")
+	queryCmd.Flags().String("order", "", "order in which a list of results should be returned (asc or desc)")
 
 	rootCmd.AddCommand(fgaCmd)
 }
 
 var fgaCmd = &cobra.Command{
-	Use:   "organization",
-	Short: "Manage organizations (create, update, delete, etc).",
-	Long:  "Create, update, and delete organizations and manage organization domain policies.",
+	Use:   "fga",
+	Short: "Manage FGA resources (object types, warrants, and objects).",
+	Long:  "Manage FGA-specific resources like object types, warrants, and objects and perform check and query operations to validate your FGA model.",
 }
 
 var listObjectTypesCmd = &cobra.Command{
@@ -152,28 +167,6 @@ var removeRelationCmd = &cobra.Command{
 	},
 }
 
-var checkRelationCmd = &cobra.Command{
-	Use:     "check <subject> <relation> <object> [context]",
-	Short:   "Check for a relation",
-	Long:    "Check if a given subject has the specified relation on a given object, optionally specifying context to use while evaluating the check.",
-	Example: `workos fga check user:john owner document:xyz '{"organization": "acme"}'`,
-	Args:    cobra.RangeArgs(3, 4),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
-	},
-}
-
-var queryCmd = &cobra.Command{
-	Use:     "query <query> [context]",
-	Short:   "Query for access rules",
-	Long:    "Run a query to see which objects a subject has access to or which subjects have access to an object, optionally specifying context to use while evaluating the query.",
-	Example: "workos fga query select document where user:john is owner",
-	Args:    cobra.RangeArgs(1, 2),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
-	},
-}
-
 var createObjectCmd = &cobra.Command{
 	Use:     "create <object> [meta]",
 	Short:   "Create a new object",
@@ -192,6 +185,10 @@ var listObjectsCmd = &cobra.Command{
 	Example: "workos fga object list --objectType=user --limit=15",
 	Args:    cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		//limit, _ := cmd.Flags().GetInt("limit")
+		//before, _ := cmd.Flags().GetString("before")
+		//after, _ := cmd.Flags().GetString("after")
+		//order, _ := cmd.Flags().GetString("order")
 		return nil
 	},
 }
@@ -216,4 +213,115 @@ var deleteObjectCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return nil
 	},
+}
+
+var checkRelationCmd = &cobra.Command{
+	Use:     "check <subject> <relation> <object> [context]",
+	Short:   "Check for a relation",
+	Long:    "Check if a given subject has the specified relation on a given object, optionally specifying context to use while evaluating the check.",
+	Example: `workos fga check user:john owner document:xyz '{"organization": "acme"}'`,
+	Args:    cobra.RangeArgs(3, 4),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		subjectType, subjectIdRelation, valid := strings.Cut(args[0], ":")
+		if !valid {
+			return fmt.Errorf("invalid subject: %s", args[0])
+		}
+		subjectId, subjectRelation, _ := strings.Cut(subjectIdRelation, "#")
+		relation := args[1]
+		objectType, objectId, valid := strings.Cut(args[2], ":")
+		if !valid {
+			return fmt.Errorf("invalid object: %s", args[0])
+		}
+
+		var checkContext map[string]interface{}
+		if len(args) > 3 {
+			err := json.Unmarshal([]byte(args[3]), &checkContext)
+			if err != nil {
+				return fmt.Errorf("invalid context: %s", args[3])
+			}
+		}
+
+		warrantToken, _ := cmd.Flags().GetString("warrantToken")
+		debug, _ := cmd.Flags().GetBool("debug")
+
+		warrantCheck := fga.WarrantCheck{
+			ObjectType: objectType,
+			ObjectId:   objectId,
+			Relation:   relation,
+			Subject: fga.Subject{
+				ObjectType: subjectType,
+				ObjectId:   subjectId,
+				Relation:   subjectRelation,
+			},
+			Context: checkContext,
+		}
+		result, err := fga.Check(
+			context.Background(),
+			fga.CheckOpts{
+				Warrant:      warrantCheck,
+				WarrantToken: warrantToken,
+				Debug:        debug,
+			},
+		)
+		if err != nil {
+			return fmt.Errorf("error performing check: %v", err)
+		}
+
+		warrantCheckString, err := warrantCheckAsString(warrantCheck)
+		if err != nil {
+			return fmt.Errorf("invalid check: %v", err)
+		}
+
+		assert, _ := cmd.Flags().GetString("assert")
+		if assert != "" {
+			assertBool, err := strconv.ParseBool(assert)
+			if err != nil {
+				return fmt.Errorf("invalid assertion: %s", assert)
+			}
+
+			if assertBool == result.Authorized() {
+				fmt.Printf("%s %s\n", termenv.String(printer.Checkmark, fmt.Sprintf("assert %t", assertBool)).Foreground(printer.Green), warrantCheckString)
+			} else {
+				fmt.Printf("%s %s\n", termenv.String(printer.Cross, fmt.Sprintf("assert %t", assertBool)).Foreground(printer.Red), warrantCheckString)
+				os.Exit(1)
+			}
+		} else if result.Authorized() {
+			fmt.Printf("%s %s\n", termenv.String(printer.Checkmark, "true").Foreground(printer.Green), warrantCheckString)
+		} else {
+			fmt.Printf("%s %s\n", termenv.String(printer.Cross, "false").Foreground(printer.Red), warrantCheckString)
+		}
+
+		return nil
+	},
+}
+
+var queryCmd = &cobra.Command{
+	Use:     "query <query> [context]",
+	Short:   "Query for access rules",
+	Long:    "Run a query to see which objects a subject has access to or which subjects have access to an object, optionally specifying context to use while evaluating the query.",
+	Example: "workos fga query select document where user:john is owner",
+	Args:    cobra.RangeArgs(1, 2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func warrantCheckAsString(w fga.WarrantCheck) (string, error) {
+	s := fmt.Sprintf(
+		"%s:%s %s %s:%s",
+		w.Subject.ObjectType,
+		w.Subject.ObjectId,
+		w.Relation,
+		w.ObjectType,
+		w.ObjectId,
+	)
+	if len(w.Context) > 0 {
+		bytes, err := json.Marshal(w.Context)
+		if err != nil {
+			return "", err
+		}
+		s = fmt.Sprintf("%s '%s'", s, string(bytes))
+	}
+
+	return s, nil
 }
